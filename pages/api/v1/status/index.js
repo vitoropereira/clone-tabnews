0b79351b23a4629e31e7730caddad7f778938b6d
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2023-12-26 02:27:43 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,33 @@
 import database from 'infra/database.js'
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1+1 as sum;') 
-  console.log(result.rows)
+  const updatedAt = new Date().toISOString()
+
+  const databaseVersionResulte = await database.query("SHOW server_version;")
+  const databaseVersionValue = databaseVersionResulte.rows[0].server_version
+
+  const databaseMaxConnectionResult = await database.query('SHOW max_connections;')
+  const databaseMaxConnectionValue = databaseMaxConnectionResult.rows[0].max_connections
+
+  const databaseName = process.env.POSTGRES_DB
+  const databaseOpenConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM  pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  })
+  const databaseOpenConnectionsValue = databaseOpenConnectionsResult.rows[0].count
+
   response
     .status(200)
-    .json({ message: "alunos do curso.dev são pessoas acima da média." });
+    .json({
+      updated_at: updatedAt,
+      dependecies: {
+        database: {
+          version: databaseVersionValue,
+          max_connections: parseInt(databaseMaxConnectionValue),
+          opened_conecctions: databaseOpenConnectionsValue
+        }
+      }
+    });
 }
 
 export default status;  
