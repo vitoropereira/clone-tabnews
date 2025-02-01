b5288fece8b149ee27fa3a43cacc1a81e8785af6
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2025-02-01 06:30:22 +0000

    feat: add initial version of ´/status´ page

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,8 +3,8 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const databaseVersionResulte = await database.query("SHOW server_version;");
-  const databaseVersionValue = databaseVersionResulte.rows[0].server_version;
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
   const databaseMaxConnectionResult = await database.query(
     "SHOW max_connections;",
@@ -22,11 +22,11 @@ async function status(request, response) {
 
   response.status(200).json({
     updated_at: updatedAt,
-    dependecies: {
+    dependencies: {
       database: {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionValue),
-        opened_conecctions: databaseOpenConnectionsValue,
+        opened_connections: databaseOpenConnectionsValue,
       },
     },
   });
