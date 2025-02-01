8b1c92bbaae460839a14384dd7432f68e9731cda
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2025-02-01 06:14:09 +0000

    feat: add initial version of ´/status´ page

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -8,9 +8,9 @@ describe("GET /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     test("Retrieving current system status", async () => {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      expect(response.status).toBe(200);
-
+      
       const responseBody = await response.json();
+      expect(response.status).toBe(200);
       expect(responseBody.updated_at).toBeDefined();
 
       const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
